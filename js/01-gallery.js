77b05c1de47891465b56d3fc61ae32682caaac51
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-25 14:54:37 +0300

    createGalleryCardsMarkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// получаем доступ к div где будет храниться коллекция
+const galleryConteiner = document.querySelector(".gallery");
+// создаем переменную где будет храниться вся галерея
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+// console.log(galleryMarkup);
+// console.log(galleryConteiner);
+// добавляем в разметку галерею
+galleryConteiner.insertAdjacentHTML("beforeend", galleryMarkup);
+// объявляем функцию создания карточек галереи
+function createGalleryCardsMarkup(galleryItems) {
+  // возвращаем созданные значения
+  return (
+    galleryItems
+      //   на ретерне вызываем мар распыляем массив
+      .map(({ preview, original, description }) => {
+        // на ретерне мэпа вставляем шаблон с значениями от распыления
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+      })
+      // соединяем шаблон функцией
+      .join("")
+  );
+}
+// console.log(galleryItems);
